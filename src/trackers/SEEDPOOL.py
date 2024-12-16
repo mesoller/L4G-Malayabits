c8d4087f0280b86eab3661d0ce08c06efff1d7e4
groggy9788 <groggy9788@proton.me>
2024-12-15 20:53:45 -0600

    Add custom category logic for seedpool
     If category is TV and the release name matches the autobrr filter below, set category to SPORTS
                   EFL*,*mlb*,*formula1*,*nascar*,*nfl*,*wrc*,*wwe*,*fifa*,*boxing*,
                   *rally*,*ufc*,*ppv*,*uefa*,*nhl*,*nba*,*motogp*,*moto2*,*moto3*,
                   *gamenight*,*darksport*,*overtake*
    If caetgory is TV and meta has non-zero tv_pack, set category to boxset

--- src/trackers/SEEDPOOL.py
+++ src/trackers/SEEDPOOL.py
@@ -2,9 +2,10 @@
 # import discord
 import asyncio
 import requests
-import distutils.util
-import os
 import platform
+from str2bool import str2bool
+import bencodepy
+import re
 
 from src.trackers.COMMON import COMMON
 from src.console import console
@@ -34,97 +35,135 @@ class SEEDPOOL():
         self.signature = None
         self.banned_groups = [""]
         pass
-    
-    async def get_cat_id(self, category_name):
+
+    ###############################################################
+    ### Update get_cat_id to take the full meta dict as an argument
+    ### If mal_id is non-zero, set category to ANIME
+    ### If category is TV and the release name matches the autobrr filter below, set category to SPORTS
+    #           EFL*,*mlb*,*formula1*,*nascar*,*nfl*,*wrc*,*wwe*,*fifa*,*boxing*,
+    #           *rally*,*ufc*,*ppv*,*uefa*,*nhl*,*nba*,*motogp*,*moto2*,*moto3*,
+    #           *gamenight*,*darksport*,*overtake*
+    ### If category is TV and meta has non-zero tv_pack, set category to BOXSET
+    async def get_cat_id(self, meta):
+        category_name = meta['category']
+        release_title = meta['name']
+        # Custom SEEDPOOL category logic
+
+        # Doing a check here means that anime movies and shows are treated as anime
+        if meta['mal_id'] != 0:
+            return '6'
+
+        if category_name == 'TV':
+            if meta['tv_pack'] != 0:
+                return '13' # BOXSET
+            if self.contains_sports_patterns(release_title):
+                return '8' # SPORTS
+
+
+        # Otherwise use the default category logic
         category_id = {
-            'MOVIE': '1', 
-            'TV': '2', 
-            }.get(category_name, '0')
+            'MOVIE': '1',
+            'TV': '2',
+        }.get(category_name, '0')
         return category_id
+
+    # New function to check for sports releases in a title
+    def contains_sports_patterns(self, release_title):
+        patterns = [
+            r'EFL.*', r'.*mlb.*', r'.*formula1.*', r'.*nascar.*', r'.*nfl.*', r'.*wrc.*', r'.*wwe.*',
+            r'.*fifa.*', r'.*boxing.*', r'.*rally.*', r'.*ufc.*', r'.*ppv.*', r'.*uefa.*', r'.*nhl.*',
+            r'.*nba.*', r'.*motogp.*', r'.*moto2.*', r'.*moto3.*', r'.*gamenight.*', r'.*darksport.*',
+            r'.*overtake.*'
+        ]
+
+        for pattern in patterns:
+            if re.search(pattern, release_title, re.IGNORECASE):
+                return True
+        return False
 
     async def get_type_id(self, type):
         type_id = {
-            'DISC': '1', 
+            'DISC': '1',
             'REMUX': '2',
-            'WEBDL': '4', 
-            'WEBRIP': '5', 
+            'WEBDL': '4',
+            'WEBRIP': '5',
             'HDTV': '6',
             'ENCODE': '3'
-            }.get(type, '0')
+        }.get(type, '0')
         return type_id
 
     async def get_res_id(self, resolution):
         resolution_id = {
-            '8640p':'10', 
-            '4320p': '1', 
-            '2160p': '2', 
-            '1440p' : '3',
+            '8640p': '10',
+            '4320p': '1',
+            '2160p': '2',
+            '1440p': '3',
             '1080p': '3',
-            '1080i':'4', 
-            '720p': '5',  
-            '576p': '6', 
+            '1080i': '4',
+            '720p': '5',
+            '576p': '6',
             '576i': '7',
-            '480p': '8', 
+            '480p': '8',
             '480i': '9'
-            }.get(resolution, '10')
+        }.get(resolution, '10')
         return resolution_id
 
     ###############################################################
-    ######   STOP HERE UNLESS EXTRA MODIFICATION IS NEEDED   ######
+    ######   STOP HERE UNLESS EXTRA MODIFICATION IS NEEDED   ###### noqa E266
     ###############################################################
 
-    async def upload(self, meta):
+    async def upload(self, meta, disctype):
         common = COMMON(config=self.config)
         await common.edit_torrent(meta, self.tracker, self.source_flag)
-        cat_id = await self.get_cat_id(meta['category'])
+        cat_id = await self.get_cat_id(meta)
         type_id = await self.get_type_id(meta['type'])
         resolution_id = await self.get_res_id(meta['resolution'])
         await common.unit3d_edit_desc(meta, self.tracker, self.signature)
         region_id = await common.unit3d_region_ids(meta.get('region'))
         distributor_id = await common.unit3d_distributor_ids(meta.get('distributor'))
-        if meta['anon'] == 0 and bool(distutils.util.strtobool(str(self.config['TRACKERS'][self.tracker].get('anon', "False")))) == False:
+        if meta['anon'] == 0 and bool(str2bool(str(self.config['TRACKERS'][self.tracker].get('anon', "False")))) is False:
             anon = 0
         else:
             anon = 1
 
-        if meta['bdinfo'] != None:
+        if meta['bdinfo'] is not None:
             mi_dump = None
             bd_dump = open(f"{meta['base_dir']}/tmp/{meta['uuid']}/BD_SUMMARY_00.txt", 'r', encoding='utf-8').read()
         else:
             mi_dump = open(f"{meta['base_dir']}/tmp/{meta['uuid']}/MEDIAINFO.txt", 'r', encoding='utf-8').read()
             bd_dump = None
-        desc = open(f"{meta['base_dir']}/tmp/{meta['uuid']}/[{self.tracker}]DESCRIPTION.txt", 'r').read()
+        desc = open(f"{meta['base_dir']}/tmp/{meta['uuid']}/[{self.tracker}]DESCRIPTION.txt", 'r', encoding='utf-8').read()
         open_torrent = open(f"{meta['base_dir']}/tmp/{meta['uuid']}/[{self.tracker}]{meta['clean_name']}.torrent", 'rb')
         files = {'torrent': open_torrent}
         data = {
-            'name' : meta['name'],
-            'description' : desc,
-            'mediainfo' : mi_dump,
-            'bdinfo' : bd_dump, 
-            'category_id' : cat_id,
-            'type_id' : type_id,
-            'resolution_id' : resolution_id,
-            'tmdb' : meta['tmdb'],
-            'imdb' : meta['imdb_id'].replace('tt', ''),
-            'tvdb' : meta['tvdb_id'],
-            'mal' : meta['mal_id'],
-            'igdb' : 0,
-            'anonymous' : anon,
-            'stream' : meta['stream'],
-            'sd' : meta['sd'],
-            'keywords' : meta['keywords'],
-            'personal_release' : int(meta.get('personalrelease', False)),
-            'internal' : 0,
-            'featured' : 0,
-            'free' : 0,
-            'doubleup' : 0,
-            'sticky' : 0,
+            'name': meta['name'],
+            'description': desc,
+            'mediainfo': mi_dump,
+            'bdinfo': bd_dump,
+            'category_id': cat_id,
+            'type_id': type_id,
+            'resolution_id': resolution_id,
+            'tmdb': meta['tmdb'],
+            'imdb': meta['imdb_id'].replace('tt', ''),
+            'tvdb': meta['tvdb_id'],
+            'mal': meta['mal_id'],
+            'igdb': 0,
+            'anonymous': anon,
+            'stream': meta['stream'],
+            'sd': meta['sd'],
+            'keywords': meta['keywords'],
+            'personal_release': int(meta.get('personalrelease', False)),
+            'internal': 0,
+            'featured': 0,
+            'free': 0,
+            'doubleup': 0,
+            'sticky': 0,
         }
         # Internal
-        if self.config['TRACKERS'][self.tracker].get('internal', False) == True:
+        if self.config['TRACKERS'][self.tracker].get('internal', False) is True:
             if meta['tag'] != "" and (meta['tag'][1:] in self.config['TRACKERS'][self.tracker].get('internal_groups', [])):
                 data['internal'] = 1
-                
+
         if region_id != 0:
             data['region_id'] = region_id
         if distributor_id != 0:
@@ -133,38 +172,34 @@ class SEEDPOOL():
             data['season_number'] = meta.get('season_int', '0')
             data['episode_number'] = meta.get('episode_int', '0')
         headers = {
-            'User-Agent': f'Upload Assistant/2.1 ({platform.system()} {platform.release()})'
+            'User-Agent': f'Upload Assistant/2.2 ({platform.system()} {platform.release()})'
         }
         params = {
-            'api_token' : self.config['TRACKERS'][self.tracker]['api_key'].strip()
+            'api_token': self.config['TRACKERS'][self.tracker]['api_key'].strip()
         }
-        
-        if meta['debug'] == False:
+
+        if meta['debug'] is False:
             response = requests.post(url=self.upload_url, files=files, data=data, headers=headers, params=params)
             try:
                 console.print(response.json())
-            except:
+            except Exception:
                 console.print("It may have uploaded, go check")
-                return 
+                return
         else:
-            console.print(f"[cyan]Request Data:")
+            console.print("[cyan]Request Data:")
             console.print(data)
         open_torrent.close()
 
-
-   
-
-
-    async def search_existing(self, meta):
+    async def search_existing(self, meta, disctype):
         dupes = []
         console.print("[yellow]Searching for existing torrents on site...")
         params = {
-            'api_token' : self.config['TRACKERS'][self.tracker]['api_key'].strip(),
-            'tmdbId' : meta['tmdb'],
-            'categories[]' : await self.get_cat_id(meta['category']),
-            'types[]' : await self.get_type_id(meta['type']),
-            'resolutions[]' : await self.get_res_id(meta['resolution']),
-            'name' : ""
+            'api_token': self.config['TRACKERS'][self.tracker]['api_key'].strip(),
+            'tmdbId': meta['tmdb'],
+            'categories[]': await self.get_cat_id(meta),
+            'types[]': await self.get_type_id(meta['type']),
+            'resolutions[]': await self.get_res_id(meta['resolution']),
+            'name': ""
         }
         if meta.get('edition', "") != "":
             params['name'] = params['name'] + f" {meta['edition']}"
@@ -176,8 +211,47 @@ class SEEDPOOL():
                 # difference = SequenceMatcher(None, meta['clean_name'], result).ratio()
                 # if difference >= 0.05:
                 dupes.append(result)
-        except:
+        except Exception:
             console.print('[bold red]Unable to search for existing torrents on site. Either the site is down or your API key is incorrect')
             await asyncio.sleep(5)
 
         return dupes
+
+    async def search_torrent_page(self, meta, disctype):
+        torrent_file_path = f"{meta['base_dir']}/tmp/{meta['uuid']}/[{self.tracker}]{meta['clean_name']}.torrent"
+        Name = meta['name']
+        quoted_name = f'"{Name}"'
+
+        params = {
+            'api_token': self.config['TRACKERS'][self.tracker]['api_key'].strip(),
+            'name': quoted_name
+        }
+
+        try:
+            response = requests.get(url=self.search_url, params=params)
+            response.raise_for_status()
+            response_data = response.json()
+
+            if response_data['data'] and isinstance(response_data['data'], list):
+                details_link = response_data['data'][0]['attributes'].get('details_link')
+
+                if details_link:
+                    with open(torrent_file_path, 'rb') as open_torrent:
+                        torrent_data = open_torrent.read()
+
+                    torrent = bencodepy.decode(torrent_data)
+                    torrent[b'comment'] = details_link.encode('utf-8')
+                    updated_torrent_data = bencodepy.encode(torrent)
+
+                    with open(torrent_file_path, 'wb') as updated_torrent_file:
+                        updated_torrent_file.write(updated_torrent_data)
+
+                    return details_link
+                else:
+                    return None
+            else:
+                return None
+
+        except requests.exceptions.RequestException as e:
+            print(f"An error occurred during the request: {e}")
+            return None
